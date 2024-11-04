3cebc74b77eafae72e1131e23307c3b37bb5fdb4
lkmdev01 <lkmdev01@gmail.com>
2024-11-04 19:42:13 -0300

    ajustes

--- src/components/MyServices.vue
+++ src/components/MyServices.vue
@@ -2,7 +2,7 @@
 <div class="bg-cinza-dark">
   <div class="d-flex justify-content-center gap-3 flex-wrap py-5 container">
     <div class="card" style="width: 18rem;">
-      <img src="..." class="card-img-top" alt="...">
+      <!-- <img src="..." class="card-img-top" alt="..."> -->
       <div class="card-body">
         <h5 class="card-title">CABELO</h5>
         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -10,7 +10,7 @@
       </div>
     </div>
     <div class="card" style="width: 18rem;">
-      <img src="..." class="card-img-top" alt="...">
+      <!-- <img src="..." class="card-img-top" alt="..."> -->
       <div class="card-body">
         <h5 class="card-title">BARBA</h5>
         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -18,7 +18,7 @@
       </div>
     </div>
     <div class="card" style="width: 18rem;">
-      <img src="..." class="card-img-top" alt="...">
+      <!-- <img src="..." class="card-img-top" alt="..."> -->
       <div class="card-body">
         <h5 class="card-title">CABELO + BARBA</h5>
         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
